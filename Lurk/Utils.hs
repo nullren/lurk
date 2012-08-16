b181573f860f377350fe2a6f6010e1fbc9f78093
nullren <chopflop@gmail.com>
2012-08-16 02:14:11 -0700

    something is happening

--- Lurk/Utils.hs
+++ Lurk/Utils.hs
@@ -6,6 +6,7 @@ module Lurk.Utils (
 
 import Data.List
 import Data.Char
+import Text.Printf
 
 -- just something stupid to fetch a list of urls in a string
 getUrls :: String -> [String]
@@ -19,6 +20,13 @@ head' [] = []
 head' s = head s
 
 -- something to make numbers have pretty things in them
-prettySize :: Integer -> String
-prettySize = show
+prettySize :: (Real a, Show a) => a -> String
+prettySize x 
+  | x > 1024^3 = printf "%.1f GB\n" (d x (1024^3)) 
+  | x > 1024^2 = printf "%.1f MB\n" (d x (1024^2))
+  | x > 1024   = printf "%.1f KB\n" (d x 1024) 
+  | otherwise  = show x
+  where
+    d :: (PrintfArg a, Fractional a, Real a1, Real a2) => a1 -> a2 -> a
+    d z y = (realToFrac z)/(realToFrac y)
 
