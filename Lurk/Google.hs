28373455d9c42fd34d7cac714741d53add728ded
nullren <chopflop@gmail.com>
2012-08-15 23:06:26 -0700

    shortened up the code a little... maybe

--- Lurk/Google.hs
+++ Lurk/Google.hs
@@ -3,7 +3,6 @@ module Lurk.Google (
   getGoogleSearchByImageUrl,
   getSearchResults,
   getSbiResults,
-  getRawSearchResults,
   extractTopText,
   extractSbiKeywords,
   extractSearchResults
@@ -16,22 +15,43 @@ import Codec.Binary.UTF8.String
 import Network.HTTP.Base
 import Control.Applicative
 
+genericSearchUrl :: String -> String -> String
+genericSearchUrl uri = (++) uri . urlEncode
+
 getGoogleSearchUrl :: String -> String
-getGoogleSearchUrl = (++) "http://google.com/search?q=" . urlEncode
+getGoogleSearchUrl = genericSearchUrl "http://google.com/search?q="
 
 getGoogleSearchByImageUrl :: String -> String
-getGoogleSearchByImageUrl = (++) "http://google.com/searchbyimage?image_url=" . urlEncode
-
-getRawSearchResults :: String -> IO String
-getRawSearchResults = getContent . getGoogleSearchUrl
+getGoogleSearchByImageUrl = genericSearchUrl "http://google.com/searchbyimage?image_url="
 
 maxSearchResults :: Int
 maxSearchResults = 3
 
+head' [] = []
+head' s = head s
+
+getGenericResults :: (String -> Maybe String)
+                  -> String
+                  -> IO [(String, Maybe String)]
+getGenericResults extractor uri = do
+  r <- getContent $ uri
+  case extractor r of
+    Nothing -> case extractSearchResults r of
+      Just s -> return s
+      Nothing -> return [("I don't know what I'm doing!", Nothing)]
+    Just s -> return [(s, Nothing)]
+
+getSbiResults :: String -> IO [(String, Maybe String)]
+getSbiResults u = getGenericResults extractSbiKeywords (getGoogleSearchByImageUrl u)
+
+-- if there is topstuff, display it, or else give search results
+getSearchResults :: String -> IO [(String,Maybe String)]
+getSearchResults u = getGenericResults extractTopText (getGoogleSearchUrl u)
+
 -- get the text inside the "topstuff" box if there is any
 extractTopText :: String -> Maybe String
 extractTopText = content . tags where
-  tags = closing . opening . canonicalizeTags . head . sections (~== "<div id=topstuff>") . parseTags
+  tags = closing . opening . canonicalizeTags . head' . sections (~== "<div id=topstuff>") . parseTags
   opening = dropWhile (not . tagOpenLit "h2" (const True))
   closing = takeWhile (not . tagCloseLit "h2")
   content = maybeText . format . innerText
@@ -43,22 +63,13 @@ extractTopText = content . tags where
 -- get keywords from the google sbi image page
 extractSbiKeywords :: String -> Maybe String
 extractSbiKeywords = content . tags where
-  tags = closing . opening . canonicalizeTags . head . sections (~== "<div id=topstuff>") . parseTags
+  tags = closing . opening . canonicalizeTags . head' . sections (~== "<div id=topstuff>") . parseTags
   opening = dropWhile (not . tagOpenLit "a" (any (\(n,v) -> n=="style" && v=="font-weight:bold;font-style:italic")))
   closing = takeWhile (not . tagCloseLit "a")
   content = maybeText . format . innerText
   format = unwords . words
   maybeText [] = Nothing
   maybeText t = Just ("Result: " ++ (encodeString t))
-
-getSbiResults :: String -> IO [(String, Maybe String)]
-getSbiResults uri = do
-  r <- getContent $ getGoogleSearchByImageUrl uri
-  case extractSbiKeywords r of
-    Nothing -> case extractSearchResults r of
-      Just s -> return s
-      Nothing -> return [("I don't know what I'm doing!", Nothing)]
-    Just s -> return [(s, Nothing)]
 
 -- return a list of page titles and urls
 extractSearchResults :: String -> Maybe [(String, Maybe String)]
@@ -67,8 +78,6 @@ extractSearchResults p = map content <$> (maybetake $ tags p) where
   -- searches for the list items in search results
   tags = listitems . closing . opening . search
   search = head' . sections (~== "<div id=search>") . canonicalizeTags . parseTags
-  head' [] = []
-  head' x = head x
   opening = dropWhile (not . tagOpenLit "ol" (const True))
   closing = takeWhile (not . tagCloseLit "ol")
   listitems x = map (takeWhile (not . tagCloseLit "li")) (sections (~== "<li class=g>") x)
@@ -84,12 +93,3 @@ extractSearchResults p = map content <$> (maybetake $ tags p) where
   maybetake [] = Nothing
   maybetake s = Just (take maxSearchResults s)
 
--- if there is topstuff, display it, or else give search results
-getSearchResults :: String -> IO [(String,Maybe String)]
-getSearchResults query = do
-  r <- getRawSearchResults query
-  case extractTopText r of
-    Nothing -> case extractSearchResults r of
-      Just s -> return s
-      Nothing -> return [("I don't know what I'm doing!", Nothing)]
-    Just s -> return [(s, Nothing)]
