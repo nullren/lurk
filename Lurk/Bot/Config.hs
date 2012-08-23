62f4e81b9413de050e784c0095abf5ae256b410c
nullren <chopflop@gmail.com>
2012-08-22 21:21:08 -0700

    added a default config

--- Lurk/Bot/Config.hs
+++ Lurk/Bot/Config.hs
@@ -3,17 +3,37 @@ module Lurk.Bot.Config where
 import Control.Monad.Reader
 import System.IO
 import System.Time
-import Network.IRC
+import Network.IRC hiding (nick,privmsg)
 
 data BotConfig = BotConfig { nick :: String
                            , username :: String
                            , realname :: String
                            , server :: String
                            , port :: Int
+                           , ssl :: Bool
+                           , nickserv :: Bool
+                           , nickservpassword :: String
                            , channels :: [String]
                            , handlers :: [Maybe Message -> Net ()]
+                           , database :: String
                            }
   
 type Net = ReaderT Bot IO
-data Bot = Bot { socket :: Handle, starttime :: ClockTime, config :: BotConfig }
- 
+data Bot = Bot { socket :: Handle
+               , starttime :: ClockTime
+               , config :: BotConfig
+               }
+
+defaultLurkBot = BotConfig
+  { nick = "lurkbot"
+  , username = "lurkbot"
+  , realname = "avocados butt"
+  , server = "chat.freenode.net"
+  , ssl = False
+  , nickserv = False
+  , nickservpassword = "nothing!"
+  , port = 6667
+  , channels = []
+  , handlers = []
+  , database = "lurk.db"
+  }
--- lurk.hs
+++ lurk.hs
@@ -7,21 +7,19 @@ import Lurk.Handler.EverythingElse (miscHandler)
 main = runBot lurkBot
 
 -- | configure your bot here!
-
-lurkBot = BotConfig "lurkbot" --nick
-                    "lurkbot" --user name
-                    "avocados butt" --real name
-                    "chat.freenode.net"  --server
-                    6667 --port
-
-                    -- channels to join
-                    [ "#avocadobonertrust"
-                    --, "#reddit-ucla"
-                    ]
-
-                    -- handlers to use
-                    [ googleHandler
-                    , slapHandler
-                    , urlHandler
-                    , miscHandler
-                    ]
+lurkBot = defaultLurkBot
+            { nick = "lurkbot"
+            , server = "chat.freenode.net"
+            , port = 6667
+            , channels = [ "#avocadobonertrust"
+                         --, "#reddit-ucla"
+                         ]
+            , nickserv = False
+            , nickservpassword = "dingdong"
+            , handlers = [ googleHandler
+                         , slapHandler
+                         , urlHandler
+                         , miscHandler
+                         ]
+            , database = "/tmp/lurk.db"
+            }
