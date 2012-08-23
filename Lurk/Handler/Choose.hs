f025e5ff90b77cb2f3a34051056b8d50950b0f9b
nullren <chopflop@gmail.com>
2012-08-23 12:47:41 -0700

    choice is a good example

--- Lurk/Handler/Choose.hs
+++ Lurk/Handler/Choose.hs
@@ -4,22 +4,22 @@ import Lurk.Bot.IRC
 import Lurk.Bot.Config
 import Control.Monad.Reader
 import Network.IRC hiding (nick, privmsg)
+
 import Data.List
 import Data.List.Split
 import System.Random
 
 pick xs = randomRIO (0, (length xs - 1)) >>= return . (xs !!)
 
-eval :: String -> String -> Net ()
-eval c x | "!c " `isPrefixOf` x = do 
-  (liftIO $ pick $ splitOn " or " (drop 3 x)) >>= privmsg c
-eval _    _                     = return () -- ignore everything else
-
 chooseHandler :: Maybe Message -> Net ()
 chooseHandler msg = do
   cfg <- asks config
   case msg of
+
     Just (Message (Just (NickName n _ _)) "PRIVMSG" (chan:mess))
-      -> eval tgt $ concat mess
+      | "!c " `isPrefixOf` (concat mess)
+      -> (liftIO . pick . (splitOn " or ")) m >>= privmsg tgt
          where tgt = if (nick cfg) `isPrefixOf` chan then n else chan
+               m = drop 3 $ concat mess
+
     _ -> return ()
