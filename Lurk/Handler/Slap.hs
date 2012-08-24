35b56d8bce7822f4a6ac7655c7447b00612b4e27
nullren <chopflop@gmail.com>
2012-08-24 03:34:40 -0700

    hlint Slap

--- Lurk/Handler/Slap.hs
+++ Lurk/Handler/Slap.hs
@@ -6,7 +6,7 @@ import Control.Monad.Reader
 import Data.List
 
 eval :: String -> String -> Net ()
-eval c x | "!slap " `isPrefixOf` x = privmsg c ("\001ACTION slaps " ++ (drop 6 x) ++ " around with a large trout!\001")
+eval c x | "!slap " `isPrefixOf` x = privmsg c ("\001ACTION slaps " ++ drop 6 x ++ " around with a large trout!\001")
 eval _    _                        = return () -- ignore everything else
 
 slapHandler msg = do
@@ -14,5 +14,5 @@ slapHandler msg = do
   case msg of
     Just (Message (Just (NickName n _ _)) "PRIVMSG" (chan:mess))
       -> eval tgt $ concat mess
-         where tgt = if (nick cfg) `isPrefixOf` chan then n else chan
+         where tgt = if nick cfg `isPrefixOf` chan then n else chan
     _ -> return ()
