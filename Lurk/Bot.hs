911a7960af6cfe8137d9baa1050940a26f395a43
nullren <chopflop@gmail.com>
2012-08-24 03:17:24 -0700

    Bot.hs hlinted

--- Lurk/Bot.hs
+++ Lurk/Bot.hs
@@ -25,7 +25,7 @@ runBot :: BotConfig -> IO ()
 runBot cfg = E.bracket (connect cfg) disconnect loop
   where
     disconnect = connClose . connInfo
-    loop s = E.catch (runReaderT run s) (\(E.SomeException e) -> putStrLn $ show e)
+    loop s = E.catch (runReaderT run s) (\(E.SomeException e) -> (print e))
 
 connect :: BotConfig -> IO Bot
 connect cfg = notify $ do
@@ -35,34 +35,31 @@ connect cfg = notify $ do
   forkIO $ forever $ getLine >>= writer conn
   return (Bot conn t cfg db)
   where
-    writer c s = (connWrite c) $ L.pack $ s ++ "\r\n"
-    notify a = E.bracket_
+    writer c s = connWrite c $ L.pack $ s ++ "\r\n"
+    notify = E.bracket_
       (printf "Connecting to %s ... " (server cfg) >> hFlush stdout)
       (putStrLn "done.")
-      a
 
 run :: Net ()
 run = do
   cfg <- asks config
   write "NICK" (nick cfg)
-  write "USER" ((username cfg)++" 0 * :" ++ (realname cfg))
+  write "USER" (username cfg ++ " 0 * :" ++ realname cfg)
   asks connInfo >>= listen
 
 listen :: ConnInfo -> Net ()
 listen h = listen_ssl h $ L.pack ""
 
 listen_ssl :: ConnInfo -> L.ByteString -> Net ()
-listen_ssl conn bs = do
-  if (L.count '\n' bs) > 0
-    then do
-      let s' = L.takeWhile (not . (=='\r')) bs
-      let s  = L.unpack s'
-      liftIO $ putStrLn s
-      if ping s then pong s else handle s
-      listen_ssl conn $ L.drop (2 + L.length s') bs
-    else do
-      out <- liftIO $ connRead conn
-      listen_ssl conn $ L.concat [bs,L.pack $ B.unpack out]
+listen_ssl conn bs = if L.count '\n' bs > 0 then 
+    do let s' = L.takeWhile (not . (=='\r')) bs
+       let s  = L.unpack s'
+       liftIO $ putStrLn s
+       if ping s then pong s else handle s
+       listen_ssl conn $ L.drop (2 + L.length s') bs
+    else
+    do out <- liftIO $ connRead conn
+       listen_ssl conn $ L.concat [bs,L.pack $ B.unpack out]
   where
     ping x = "PING :" `isPrefixOf` x
     pong x = write "PONG" (':' : drop 6 x)
