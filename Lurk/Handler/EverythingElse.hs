e1fe71a4833526af1a3121c4b02548c4bee9b4ba
nullren <chopflop@gmail.com>
2012-08-24 03:33:26 -0700

    hlint EverythingElse

--- Lurk/Handler/EverythingElse.hs
+++ Lurk/Handler/EverythingElse.hs
@@ -7,7 +7,7 @@ import Lurk.Bot.IRC
 import System.Exit
 
 eval :: String -> String -> Net ()
-eval _    "!quit"                  = write "QUIT" ":Exiting" >> liftIO (exitWith ExitSuccess)
+eval _    "!quit"                  = write "QUIT" ":Exiting" >> liftIO exitSuccess
 eval c x | "!id " `isPrefixOf` x   = privmsg c (drop 4 x)
 eval _    _                        = return () -- ignore everything else
 
@@ -16,12 +16,12 @@ miscHandler msg = do
   cfg <- asks config
   case msg of
     Just (Message _ "376" _) 
-      -> if (nickserv cfg)
-         then privmsg "NickServ" $ "identify " ++ (nickservpassword cfg)
-         else mapM_ (\x -> write "JOIN" x) (channels cfg)
-    Just (Message _ "396" _) -> mapM_ (\x -> write "JOIN" x) (channels cfg)
+      -> if nickserv cfg
+         then privmsg "NickServ" $ "identify " ++ nickservpassword cfg
+         else mapM_ (write "JOIN") (channels cfg)
+    Just (Message _ "396" _) -> mapM_ (write "JOIN") (channels cfg)
     Just (Message _ "433" _) -> write "NICK" (nick cfg ++ "_")
     Just (Message (Just (NickName n _ _)) "PRIVMSG" (chan:mess))
       -> eval tgt $ concat mess
-         where tgt = if (nick cfg) `isPrefixOf` chan then n else chan
+         where tgt = if nick cfg `isPrefixOf` chan then n else chan
     _ -> return ()
