667afee515f2c3984553f40c1d44f1ef0a02f0ff
nullren <chopflop@gmail.com>
2012-08-24 01:01:26 -0700

    added new connect file and types file

---
+++ Lurk/Connect.hs
@@ -0,0 +1,36 @@
+module Lurk.Connect where
+
+import Crypto.Random
+import Lurk.Types
+import Network
+import Network.TLS
+import Network.TLS.Extra
+import qualified Control.Exception as E
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import System.IO
+
+-- | Make a SSL connection to a host
+connect_ssl :: String -> Int -> IO ConnInfo
+connect_ssl host port = do
+  gen <- newGenIO :: IO SystemRandom
+  ctx <- connectionClient host (show port) params gen
+  E.catch (handshake ctx) (\(E.SomeException e) -> putStrLn $ show e)
+  return ConnInfo
+    { connRead = recvData ctx
+    , connWrite = sendData ctx
+    , connClose = bye ctx
+    }
+  where
+    params = defaultParamsClient {pCiphers = ciphersuite_all}
+
+-- | Make a plaintext connection to a host
+connect_reg :: String -> Int -> IO ConnInfo
+connect_reg host port = do
+  h <- connectTo host (PortNumber . fromIntegral $ port)
+  hSetBuffering h NoBuffering
+  return ConnInfo
+    { connRead = B.hGetContents h
+    , connWrite = L.hPutStr h
+    , connClose = hClose h
+    }
---
+++ Lurk/Types.hs
@@ -0,0 +1,70 @@
+module Lurk.Types
+  ( ConnInfo(..)
+  , BotConfig(..)
+  , Bot(..)
+  , Net
+  , defaultLurkBot
+
+  -- Network.IRC
+  , Message(..)
+  , Prefix(..)
+  , Parameter
+  , ServerName
+  , UserName
+  , RealName
+  , Command
+  ) where
+
+import Control.Monad.Reader
+import Database.HDBC
+import Database.HDBC.Sqlite3
+import Network.IRC hiding (nick,privmsg)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import System.IO
+import System.Time
+
+data ConnInfo = ConnInfo
+  { connRead :: IO B.ByteString
+  , connWrite :: L.ByteString -> IO ()
+  , connClose :: IO ()
+  }
+
+data BotConfig = BotConfig 
+  { nick :: String
+  , username :: String
+  , realname :: String
+  , server :: String
+  , port :: Int
+  , ssl :: Bool
+  , nickserv :: Bool
+  , nickservpassword :: String
+  , channels :: [String]
+  , handlers :: [Maybe Message -> Net ()]
+  , logging :: Bool
+  , database :: String
+  }
+
+data Bot = Bot 
+  { connInfo :: ConnInfo
+  , startTime :: ClockTime
+  , config :: BotConfig
+  , db :: Maybe Connection
+  }
+
+type Net = ReaderT Bot IO
+
+defaultLurkBot = BotConfig
+  { nick = "lurkbot"
+  , username = "lurkbot"
+  , realname = "avocados butt"
+  , server = "chat.freenode.net"
+  , ssl = False
+  , nickserv = False
+  , nickservpassword = "nothing!"
+  , port = 6667
+  , channels = []
+  , handlers = []
+  , logging = False
+  , database = "lurk.db"
+  }
