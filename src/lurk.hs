fe27e69f7a2f1c23a019f8005af387f8c1a9834d
nullren <chopflop@gmail.com>
2012-08-26 11:15:29 -0700

    changed lurk.hs to be smaller

--- src/lurk.hs
+++ src/lurk.hs
@@ -6,22 +6,15 @@ main = runBot lurkBot
 
 -- | configure your bot here!
 lurkBot = defaultLurkBot
-            { nick = "lurkbot"
-            , server = "irc.starfyre.net"
-            , port = 6697
-            , ssl = True
-            , channels = [ "#mathematics"
-                         --, "#reddit-ucla"
-                         ]
-            , nickserv = False
-            , nickservpassword = "dingdong"
-            , handlers = [ logHandler
-                         , googleHandler
-                         , slapHandler
-                         , chooseHandler
-                         , miscHandler
-                         , urlHandler
-                         ]
-            , database = "/tmp/lurk.db"
-            , logging = False
-            }
+  { nick                = "lurkbot"
+  , server              = "irc.fantasy.net"
+  , port                = 6697
+  , ssl                 = True
+  , channels            = [ "#cooldudes"
+                          , "#spam"
+                          ]
+  , nickserv            = False
+  , nickservpassword    = "password"
+  , database            = "/tmp/lurk.db"
+  , logging             = False
+  }
