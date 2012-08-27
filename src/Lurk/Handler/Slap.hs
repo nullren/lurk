f869d2ede156253d910728ea86be3bea64a5f6c4
nullren <chopflop@gmail.com>
2012-08-26 17:04:05 -0700

    slap a bitch

--- src/Lurk/Handler/Slap.hs
+++ src/Lurk/Handler/Slap.hs
@@ -1,18 +1,19 @@
 module Lurk.Handler.Slap (slapHandler) where
 
-import Control.Monad.Reader
 import Data.List
-import Lurk.Bot.IRC
 import Lurk.Types
+import Lurk.Handler
 
-eval :: String -> String -> Net ()
-eval c x | "!slap " `isPrefixOf` x = privmsg c ("\001ACTION slaps " ++ drop 6 x ++ " around with a large trout!\001")
-eval _    _                        = return () -- ignore everything else
 
-slapHandler msg = do
-  cfg <- asks config
-  case msg of
-    Just (Message (Just (NickName n _ _)) "PRIVMSG" (chan:mess))
-      -> eval tgt $ concat mess
-         where tgt = if nick cfg `isPrefixOf` chan then n else chan
-    _ -> return ()
+slap = msgHandler
+  { condition = isPrefixOf "!slap "
+  , response = \(n, m) -> return . justSlap . victims $ m
+  }
+
+justSlap [] = Nothing
+justSlap ms = Just . build $ ms
+victims = tail . words
+act s = "\001ACTION slaps " ++ s ++ " around with a large trout!\001"
+build = act . unwords
+
+slapHandler = handler slap
